a4900d1da44a89617604ee42db432d151808533d
César A. Suárez Orizondo <ca.suarex.ext@agbar.ga.local>
2025-01-09 14:01:19 +0000

    Actualizar la versión de Astro en package.json, habilitar la barra de herramientas de desarrollo en astro.config.mjs y reemplazar la etiqueta <img> por el componente <Image> en index.astro.

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import vercel from "@astrojs/vercel";
 // https://astro.build/config
 export default defineConfig({
   devToolbar: {
-    enabled: false,
+    enabled: true,
   },
   integrations: [tailwind(), react()],
   output: "server",
